3c5b737838f843dab06324be5461ac80c20f61e1
bakissisoungatech <bakissisoungatech@gmail.com>
2026-07-16 13:06:55 +0100

    first commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: [
+      'bakissi.up.railway.app',          // Autorise spécifiquement ton domaine actuel
+      '.up.railway.app'                  // Autorise tous les sous-domaines Railway par sécurité
+    ]
+  }
 })
