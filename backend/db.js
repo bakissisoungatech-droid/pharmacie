1bd1a9d8fc6b9fe10525e4db037f5001ea6b8083
bakissisoungatech <bakissisoungatech@gmail.com>
2026-07-20 11:38:34 +0100

    first commit

--- backend/db.js
+++ backend/db.js
@@ -1,13 +1,31 @@
 const { Pool } = require("pg");
 require('dotenv').config();
 
+// On récupère l'URL complète de la base de données
+const connectionString = process.env.DATABASE_URL || process.env.DATABASE_PUBLIC_URL;
 
 const pool = new Pool({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT
+  connectionString: connectionString,
+  ssl: {
+    rejectUnauthorized: false // OBLIGATOIRE pour accepter le certificat SSL de Railway
+  },
+  connectionTimeoutMillis: 5000 // Abandonne au bout de 5 sec si la BD ne répond pas au lieu de bloquer des minutes
 });
 
-module.exports = pool;  
+// const pool = new Pool({
+//   user: process.env.PG_USER,
+//   host: process.env.PG_HOST,
+//   database: process.env.PG_DATABASE,
+//   password: process.env.PG_PASSWORD,
+//   port: process.env.PG_PORT
+// });
+
+module.exports = pool;
+
+// const { Pool } = require("pg");
+// require('dotenv').config();
+
+
+
+
+// module.exports = pool;  
