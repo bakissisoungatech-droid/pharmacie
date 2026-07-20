13f8aa76a0d61634e8dc0bb3ba278422e819c51f
bakissisoungatech <bakissisoungatech@gmail.com>
2026-07-20 10:34:15 +0100

    first commit

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,5 +1,8 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://pharmacie-production-9a16.up.railway.app/");
+const socket = io("https://pharmacie-production-9a16.up.railway.app", {
+  transports: ["websocket", "polling"],
+  withCredentials: true
+});
 
 export default socket;
